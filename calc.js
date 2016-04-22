34bced889e73b9e4c051e134479810e744e3e5b5
Surya Surendranath <surya.snath08@gmail.com>
2016-04-22 13:29:44 +1200

    working calc

--- calc.js
+++ calc.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded',function(){
 
 var keys=document.querySelectorAll('#calculator span');
 
@@ -6,6 +7,7 @@ for(var i=0;i<keys.length;i++){
 
     var input=document.querySelector('.screen');
     var inputVal=input.innerHTML;
+   console.log(inputVal)
     var btnVal=this.innerHTML;
 
     if(btnVal=='C'){
@@ -19,8 +21,35 @@ for(var i=0;i<keys.length;i++){
       var equation=inputVal;
       equation=equation.replace(/x/g,'*');
 
-      if(equation)
+      if(equation){
       input.innerHTML=eval(equation);
+    decimalAdded=false;
     }
-  }
+
+    else if(operators.indexOf(btnVal)>-1){
+      var lastChar=inputVal[inputVal.length-1];
+
+      if(inputVal!="" && operators.indexOf(lastChar)==-1){
+              input.innerHTML+=btnVal;
+      }
+      
+      if(operators.indexOf(lastChar)>-1 && inputVallength >1){
+        input.innerHTML=inputVal.replace(/.$/,btnVal);
+      }
+     
+    }
+    else if(btnVal=="."){
+      if(!decimalAdded){
+        input.innerHTML+=btnVal;
+        decimalAdded=true;
+      }
+    }
+else{
+  input.innerHTML+=btnVal;
 }
+      
+   } 
+  
+}
+}
+})
